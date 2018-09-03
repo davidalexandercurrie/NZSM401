c934b02bf7d805829cc9caa3396eee84a3d1a9aa
David Currie <david.alexander.currie@gmail.com>
2018-09-04 00:51:14 +1200

    process.env

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(3000);
+
 var io = socket(server);
 
-app.get(‘/’, function (req, res) {
- res.send(express.static('public'));
-});
-
-console.log("My socket server is running");
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("My socket server is running");
+})
 
 io.sockets.on('connection', newConnection);
 
