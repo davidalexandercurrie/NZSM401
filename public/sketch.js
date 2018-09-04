d53d70e54bd81578f08c42d619e74291cbbd40a6
David Currie <david.alexander.currie@gmail.com>
2018-09-04 12:06:41 +1200

    removed cloudflare links from index.html

--- public/sketch.js
+++ public/sketch.js
@@ -1,34 +1,32 @@
 var video;
 var socket;
 var vScale = 16;
+socket = io.connect('http://' + document.domain + ':'+ location.port);
 
 function setup() {
   createCanvas(640, 480);
   pixelDensity(1);
   video = createCapture(VIDEO);
-
   video.size(width/vScale, height/vScale);
-  video.hide();
-
-  socket = io.connect(window.location.hostname);
+  video.loadPixels();
+  //video.hide();
 }
 
 function draw() {
-video.loadPixels();
-loadPixels();
-for(var y = 0; y < video.height; y++){
-  for(var x = 0; x < video.width; x++){
-    var index = (x + y * video.width)*4;
-    var r = video.pixels[index+0];
-    var g = video.pixels[index+1];
-    var b = video.pixels[index+2];
+  background(51);
+  video.loadPixels();
 
-    var bright = (r+g+b)/3;
-    fill(bright);
-    rect(width-(x+1)*vScale, y*vScale, vScale, vScale);
+  for(var y = 0; y < video.height; y++){
+    for(var x = 0; x < video.width; x++){
+      var index = (x + y * video.width)*4;
+      var r = video.pixels[index+0];
+      var g = video.pixels[index+1];
+      var b = video.pixels[index+2];
 
+      var bright = (r+g+b)/3;
+      fill(bright);
+      rect(width-(x+1)*vScale, y*vScale, vScale, vScale);
 
+    }
   }
-}
-
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var socket = require('socket.io');
-
+var path = require('path');
 
 var app = express();
 var server = app.listen(process.env.PORT || 3000, process.env.IP, function(){
@@ -8,9 +8,7 @@ var server = app.listen(process.env.PORT || 3000, process.env.IP, function(){
 })
 var io = socket(server);
 
-
-
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', newConnection);
 
