0a067a3d42e4a926037613450d48fdfbaa8ecd7c
David Currie <david.alexander.currie@gmail.com>
2018-09-12 22:57:46 +1200

    added socketID to message being sent from client

--- public/sketch.js
+++ public/sketch.js
@@ -5,7 +5,8 @@ var avgBright;
 var counter;
 var oldBright;
 var difference;
-var sessionid
+var socketID
+
 
 
 function setup() {
@@ -17,44 +18,47 @@ function setup() {
   video.hide();
   socket = io.connect();
   socket.on('connect', function() {
-  sessionid = socket.socket.sessionid;
-});
-}
+    socketID = socket.id;
+    console.log(socket.id);
+  });
 
-function draw() {
-  background(51);
-  video.loadPixels();
-  oldBright = avgBright;
-  avgBright = 0;
-  counter = 0;
-  for(var y = 0; y < video.height; y++){
-    for(var x = 0; x < video.width; x++){
-      var index = (x + y * video.width)*4;
-      var r = video.pixels[index+0];
-      var g = video.pixels[index+1];
-      var b = video.pixels[index+2];
+  }
 
-      var bright = (r+g+b)/3;
-      avgBright = avgBright + bright;
-      fill(bright);
-      rect(width-(x+1)*vScale, y*vScale, vScale, vScale);
-      counter++;
+  function draw() {
+    background(51);
+    video.loadPixels();
+    oldBright = avgBright;
+    avgBright = 0;
+    counter = 0;
+    for(var y = 0; y < video.height; y++){
+      for(var x = 0; x < video.width; x++){
+        var index = (x + y * video.width)*4;
+        var r = video.pixels[index+0];
+        var g = video.pixels[index+1];
+        var b = video.pixels[index+2];
+
+        var bright = (r+g+b)/3;
+        avgBright = avgBright + bright;
+        fill(bright);
+        rect(width-(x+1)*vScale, y*vScale, vScale, vScale);
+        counter++;
+      }
     }
+    avgBright = avgBright/counter;
+    difference = oldBright - avgBright;
+
+    if(Math.abs(difference) > 0.1){
+      sendAvgBright();
+    }
+
   }
-  avgBright = avgBright/counter;
-  difference = oldBright - avgBright;
 
-  if(Math.abs(difference) > 0.1){
-    sendAvgBright();
+
+  function sendAvgBright(){
+    console.log(avgBright);
+    var data = {
+      avgBrightness: avgBright,
+      Socket_ID: socketID
+    }
+    socket.emit('bright', data);
   }
-
-}
-
-
-function sendAvgBright(){
-  console.log(avgBright);
-  var data = {
-    avgBrightness: avgBright
-  }
-  socket.emit('bright', data, sessionid);
-}
