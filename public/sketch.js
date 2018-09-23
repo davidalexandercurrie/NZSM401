071170e5230ccb2fb0c12cd70482de5f2efb9102
David Currie <david.alexander.currie@gmail.com>
2018-09-23 14:11:56 +1200

    audio test

--- public/sketch.js
+++ public/sketch.js
@@ -5,60 +5,67 @@ var avgBright;
 var counter;
 var oldBright;
 var difference;
-var socketID
+var socketID;
 
+var maxiAudio = new maximJs.maxiAudio();
+var myWave = new maximJs.maxiOsc();
+maxiAudio.init();
+var freq = 220;
 
+maxiAudio.play = function () {
+  this.output = myWave.sinewave(freq);
+};
 
 function setup() {
   createCanvas(640, 480);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(width/vScale, height/vScale);
+  video.size(width / vScale, height / vScale);
 
   video.hide();
   socket = io.connect();
-  socket.on('connect', function() {
+  socket.on('connect', function () {
     socketID = socket.id;
     console.log(socket.id);
   });
 
+}
+
+function draw() {
+  background(51);
+  video.loadPixels();
+  oldBright = avgBright;
+  avgBright = 0;
+  counter = 0;
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (x + y * video.width) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+
+      var bright = (r + g + b) / 3;
+      avgBright = avgBright + bright;
+      fill(bright);
+      rect(width - (x + 1) * vScale, y * vScale, vScale, vScale);
+      counter++;
+    }
+  }
+  avgBright = avgBright / counter;
+  difference = oldBright - avgBright;
+
+  if (Math.abs(difference) > 0.1) {
+    sendAvgBright();
   }
 
-  function draw() {
-    background(51);
-    video.loadPixels();
-    oldBright = avgBright;
-    avgBright = 0;
-    counter = 0;
-    for(var y = 0; y < video.height; y++){
-      for(var x = 0; x < video.width; x++){
-        var index = (x + y * video.width)*4;
-        var r = video.pixels[index+0];
-        var g = video.pixels[index+1];
-        var b = video.pixels[index+2];
-
-        var bright = (r+g+b)/3;
-        avgBright = avgBright + bright;
-        fill(bright);
-        rect(width-(x+1)*vScale, y*vScale, vScale, vScale);
-        counter++;
-      }
-    }
-    avgBright = avgBright/counter;
-    difference = oldBright - avgBright;
-
-    if(Math.abs(difference) > 0.1){
-      sendAvgBright();
-    }
-
-  }
+}
 
 
-  function sendAvgBright(){
-    console.log(avgBright);
-    var data = {
-      avgBrightness: avgBright,
-      Socket_ID: socketID
-    }
-    socket.emit('bright', data);
-  }
+function sendAvgBright() {
+  console.log(avgBright);
+  var data = {
+    avgBrightness: avgBright,
+    Socket_ID: socketID
+  };
+  socket.emit('bright', data);
+}
