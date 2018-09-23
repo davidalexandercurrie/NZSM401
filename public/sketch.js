8bcb19464e341fd8fad1c27f96d67f65f9f2a7a8
David Currie <david.alexander.currie@gmail.com>
2018-09-23 17:02:35 +1200

    adding audio

--- public/sketch.js
+++ public/sketch.js
@@ -6,13 +6,15 @@ var counter;
 var oldBright;
 var difference;
 var socketID;
+var counter = 0;
+var freq = 400;
 
 var maxiAudio = new maximJs.maxiAudio();
 var myWave = new maximJs.maxiOsc();
 maxiAudio.init();
 
 maxiAudio.play = function () {
-  this.output = myWave.sinewave(avgBright);
+  this.output = myWave.sinewave(freq) * 0.5;
 };
 
 function setup() {
@@ -31,40 +33,48 @@ function setup() {
 }
 
 function draw() {
-  background(51);
-  video.loadPixels();
-  oldBright = avgBright;
-  avgBright = 0;
-  counter = 0;
-  for (var y = 0; y < video.height; y++) {
-    for (var x = 0; x < video.width; x++) {
-      var index = (x + y * video.width) * 4;
-      var r = video.pixels[index + 0];
-      var g = video.pixels[index + 1];
-      var b = video.pixels[index + 2];
+  counter++;
+  if (counter % 5 == 0) {
+    video.loadPixels();
+    oldBright = avgBright;
+    avgBright = 0;
+    counter = 0;
+    for (var y = 0; y < video.height; y++) {
+      for (var x = 0; x < video.width; x++) {
+        var index = (x + y * video.width) * 4;
+        var r = video.pixels[index + 0];
+        var g = video.pixels[index + 1];
+        var b = video.pixels[index + 2];
 
-      var bright = (r + g + b) / 3;
-      avgBright = avgBright + bright;
-      fill(bright);
-      rect(width - (x + 1) * vScale, y * vScale, vScale, vScale);
-      counter++;
+        var bright = (r + g + b) / 3;
+        avgBright = avgBright + bright;
+        fill(bright);
+        rect(width - (x + 1) * vScale, y * vScale, vScale, vScale);
+        counter++;
+      }
     }
-  }
-  avgBright = avgBright / counter;
-  difference = oldBright - avgBright;
+    avgBright = avgBright / counter;
+    difference = oldBright - avgBright;
 
-  if (Math.abs(difference) > 0.2) {
-    sendAvgBright();
+    if (Math.abs(difference) > 0.2) {
+      sendAvgBright();
+      console.log(avgBright);
+      updateFreq(avgBright);
+    }
   }
 
 }
 
 
 function sendAvgBright() {
-  console.log(avgBright);
+  //console.log(avgBright);
   var data = {
     avgBrightness: avgBright,
     Socket_ID: socketID
   };
   socket.emit('bright', data);
+}
+
+function updateFreq(frequency) {
+  freq = frequency;
 }
